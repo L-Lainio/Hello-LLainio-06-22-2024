a2f14513115005324c25815ce33ad8f0c7b4e92c
L-Lainio <arollainio@gmail.com>
2024-06-22 22:16:25 -0400

    updating err

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,8 +3,8 @@ import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa6";
 
 function About() {
     return (
-        <div className='mt-12 px-44 text-center flex flex-col items-center'>
-            <h2 className='text-[40px] font-bold'>{Strings.ABOUT_HEADING}</h2>
+        <div className='mt-9 px-25 text-center flex flex-col items-center'>
+            <h2 className='text-[3rem] font-bold'>{Strings.ABOUT_HEADING}</h2>
             <FaQuoteLeft className='bg-purple-500 p-3 text-[44px] rounded-full mt-6 text-white'/>
             <h2 className='my-5 text-gray-400 text-[14px]'>{Strings.ABOUT_DESC}</h2>
             <FaQuoteRight className='bg-purple-500 p-3 text-[44px] rounded-full mt-6 text-white'/>
--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -23,7 +23,12 @@ function Services() {
     ];
 
     return (
-        <div className='px-12 mt-12'>
+        <div className='px-12 mt-12 bg-image'
+        style={{
+            padding: '10px',
+            margin: '1.5px'
+        }}
+        >
             <div className='flex items-center'>
                 <div className="w-[20px] h-[7px] bg-red-600 rounded-full"></div>
                 <div className='w-full border-[1px] mx-4 mt-[-2px]'></div>
@@ -33,12 +38,12 @@ function Services() {
             </div>
             <div className='flex justify-around mt-16'>
                 {servicesList.map((service) => (
-                    <div key={service.id} className='bg-lavender round w-[] h[]'>
-                        <div className='bg-lavender rounded-full w-[80px] h-[80px] flex items-center justify-center'>
+                    <div key={service.id} className='bg-lavender round w-[] h[] flex flex-col items-center'>
+                        <div className='bg-lavender rounded-full w-[60px] h-[100px] flex items-center justify-center'>
                             {service.logo}
                         </div>
-                        <h2 className='mt-5 font-bold'>{service.title}</h2>
-                        <h2 className='text-gray-400'>{service.desc}</h2>
+                        <h2 className='mt-5 font-bold text-center'>{service.title}</h2>
+                        <h2 className='text-gray-400 text-center'>{service.desc}</h2>
                     </div>
                 ))}
             </div>
